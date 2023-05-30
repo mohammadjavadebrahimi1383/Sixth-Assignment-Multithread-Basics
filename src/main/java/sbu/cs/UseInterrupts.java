7bb779da5d50d818acc15193d56b0bc6e43d51dc
mohamadjavadebr <ejavad128@gmail.com>
2023-05-30 22:13:26 +0330

    project done

--- src/main/java/sbu/cs/UseInterrupts.java
+++ src/main/java/sbu/cs/UseInterrupts.java
@@ -12,13 +12,13 @@ package sbu.cs;
 
 public class UseInterrupts
 {
-/*
-    TODO
-     Analyse the following class and add new code where necessary.
-     If an object from this type of thread is Interrupted, it must print this:
-        "{ThreadName} has been interrupted"
-     And then terminate itself.
- */
+    /*
+        TODO
+         Analyse the following class and add new code where necessary.
+         If an object from this type of thread is Interrupted, it must print this:
+            "{ThreadName} has been interrupted"
+         And then terminate itself.
+     */
     public static class SleepThread extends Thread {
         int sleepCounter;
 
@@ -30,31 +30,39 @@ public class UseInterrupts
         @Override
         public void run() {
             System.out.println(this.getName() + " is Active.");
-
+            boolean breaking = false;
             while (this.sleepCounter > 0)
             {
+                if (Thread.interrupted()) {
+                    System.out.println(this.getName() + " has been interrupted");
+                    break;
+                }
+
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
-
-                }
-                finally {
+                    breaking = true;
+                    break;
+                } finally {
                     this.sleepCounter--;
                     System.out.println("Number of sleeps remaining: " + this.sleepCounter);
+                    if (breaking) {
+                        System.out.println(this.getName() + " has been interrupted");
+                    }
                 }
             }
 
         }
     }
 
-/*
-    TODO
-     Analyse the following class and add new code where necessary.
-     If an object from this type of thread is Interrupted, it must print this:
-        "{ThreadName} has been interrupted"
-     And then terminate itself.
-     (Hint: Use the isInterrupted() method)
- */
+    /*
+        TODO
+         Analyse the following class and add new code where necessary.
+         If an object from this type of thread is Interrupted, it must print this:
+            "{ThreadName} has been interrupted"
+         And then terminate itself.
+         (Hint: Use the isInterrupted() method)
+     */
     public static class LoopThread extends Thread {
         int value;
         public LoopThread(int value) {
@@ -68,26 +76,42 @@ public class UseInterrupts
 
             for (int i = 0; i < 10; i += 3)
             {
+                if (this.isInterrupted()) {
+                    System.out.println(this.getName() + " has been interrupted");
+                    break;
+                }
                 i -= this.value;
-
             }
         }
     }
 
-/*
-    You can add new code to the main function. This is where you must utilize interrupts.
-    No existing line of code should be changed or deleted.
- */
+    /*
+        You can add new code to the main function. This is where you must utilize interrupts.
+        No existing line of code should be changed or deleted.
+     */
     public static void main(String[] args) {
         SleepThread sleepThread = new SleepThread(5);
         sleepThread.start();
 
         // TODO  Check if this thread runs for longer than 3 seconds (if it does, interrupt it)
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            sleepThread.interrupt();
+        }
 
         LoopThread loopThread = new LoopThread(3);
         loopThread.start();
 
         // TODO  Check if this thread runs for longer than 3 seconds (if it does, interrupt it)
-
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            loopThread.interrupt();
+        }
     }
 }
